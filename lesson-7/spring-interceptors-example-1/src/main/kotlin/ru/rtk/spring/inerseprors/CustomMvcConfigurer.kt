c036c1458876ecf6df4888a4d21e059986faa76a
natasha <n.sosnina1994@yandex.ru>
2024-10-06 19:11:06 +0500

    fix lesson 7

--- lesson-7/spring-interceptors-example-1/src/main/kotlin/ru/rtk/spring/inerseprors/CustomMvcConfigurer.kt
+++ lesson-7/spring-interceptors-example-1/src/main/kotlin/ru/rtk/spring/inerseprors/CustomMvcConfigurer.kt
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Component
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+/**
+ * Да, мы немного залезли в наш WebMvcConfigurer
+ */
 @Component
-class MyInterceptor(private val requestInterceptor: RequestInterceptor) : WebMvcConfigurer {
+class CustomMvcConfigurer(private val requestInterceptor: RequestInterceptor) : WebMvcConfigurer {
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(requestInterceptor).order(1).excludePathPatterns(
             "/metrics",
